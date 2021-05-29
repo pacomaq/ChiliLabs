55280f3edce7a6c3be732cea050c7cbf9de774e8
AlterJuice <alterjuice1758@gmail.com>
2021-05-29 15:35:41 +0300

    Edit GifAdapter with implementing new interfaces: OnBottomReach, OnGifClicked. Removing clear/set items methods. Advance bind logic

--- app/src/main/java/com/alterjuice/jgiphy/adapters/GifAdapter.java
+++ app/src/main/java/com/alterjuice/jgiphy/adapters/GifAdapter.java
@@ -5,19 +5,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alterjuice.jgiphy.databinding.ListItemGifBinding;
 import com.alterjuice.jgiphy.model.giphy.Gif;
+import com.alterjuice.jgiphy.model.giphy.Image;
 import com.alterjuice.jgiphy.utils.ImageUtils;
 
 import java.util.Collection;
 import java.util.LinkedList;
 
-public class GifAdapter extends RecyclerView.Adapter<GifAdapter.GifViewHolder> {
+public class GifAdapter extends RecyclerView.Adapter<GifAdapter.GifViewHolder> implements BaseAdapter<Gif> {
     LinkedList<Gif> collection = new LinkedList<>();
 
+    public OnBottomReachedListener onBottomReachedListener;
+    public OnGifClickedListener onGifClickedListener;
+    private ConstraintSet constraintSet = new ConstraintSet();
+    RecyclerView recyclerView;
+    public Image getImageForPreview(Gif gif){ return gif.images.fixedWidth; }
 
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
 
     @NonNull
     @Override
@@ -30,41 +42,71 @@ public class GifAdapter extends RecyclerView.Adapter<GifAdapter.GifViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull GifViewHolder holder, int position) {
-        holder.bind(collection.get(position));
-
+        if (position == getItemCount()-1){
+            if (onBottomReachedListener != null)
+                onBottomReachedListener.onBottomReached();
+        }
+        Gif bindGif = collection.get(position);
+        Image bindImage = bindGif.getPreviewImage();
+        holder.bind(bindGif);
+        ImageUtils.loadGifIntoView(bindImage.url, holder.binding.gifImage);
+        // constraintSet.clone(holder.binding.gifConstraint);
+        // constraintSet.setDimensionRatio(holder.binding.gifImage.getId(), bindImage.getImageRatio());
+        // constraintSet.applyTo(holder.binding.gifConstraint);
     }
+
 
     @Override
-    public int getItemCount() {
-        return collection.size();
-    }
+    public int getItemCount() { return collection.size(); }
+
     public void addItem(Gif gif) {
         collection.add(gif);
         notifyItemChanged(collection.size()-1);
         // notifyDataSetChanged();
     }
 
+    @Override
+    public void updateWithStartPosition(Collection<Gif> items, int position) {
+        update(items);
+        recyclerView.getLayoutManager().scrollToPosition(position);
+    }
+
+    @Override
+    public void update(Collection<Gif> items) {
+        int sizeBeforeInsert = collection.size();
+        collection.addAll(items);
+        notifyItemRangeChanged(sizeBeforeInsert, items.size());
+    }
+
+    @Override
     public void clearItems() {
         collection.clear();
         notifyDataSetChanged();
     }
 
-    public void setItems(Collection<Gif> gifs) {
-        this.collection = new LinkedList<>(gifs);
-        notifyDataSetChanged();
-    }
-
-    public class GifViewHolder extends RecyclerView.ViewHolder{
+    public class GifViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ListItemGifBinding binding;
+        Gif gif;
 
         public GifViewHolder(@NonNull View itemView, ListItemGifBinding binding) {
             super(itemView);
             this.binding = binding;
+            itemView.setOnClickListener(this);
         }
+
+
         public void bind(Gif gif){
+            this.gif = gif;
             binding.setGif(gif);
             binding.executePendingBindings();
-            ImageUtils.loadGifIntoView(gif.images.fixedHeight.url, binding.gifImage);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (onGifClickedListener != null){
+                onGifClickedListener.onGifClicked(gif, collection.indexOf(gif));
+            }
+
         }
     }
 
