2cf2121c628e95935ebab3992f43ca03f4f75c9f
AlterJuice <alterjuice1758@gmail.com>
2021-05-30 12:20:05 +0300

    Add gif to GifFragment, add onClickListener to handle copying url

--- app/src/main/java/com/alterjuice/jgiphy/ui/GifFragment.java
+++ app/src/main/java/com/alterjuice/jgiphy/ui/GifFragment.java
@@ -1,9 +1,12 @@
 package com.alterjuice.jgiphy.ui;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,9 @@ import androidx.fragment.app.Fragment;
 import com.alterjuice.jgiphy.R;
 import com.alterjuice.jgiphy.databinding.GifFragmentBinding;
 import com.alterjuice.jgiphy.model.giphy.Gif;
+import com.alterjuice.jgiphy.utils.ImageUtils;
 
 public class GifFragment extends Fragment {
-    private static final String KEY_GIF_ID = "id";
-    private static final String KEY_GIF_ORIGINAL_URL = "original_url";
     private static final String KEY_GIF = "arg:gif";
 
     private GifFragmentBinding binding;
@@ -26,9 +28,21 @@ public class GifFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.gif_fragment, container, false);
+        binding.getRoot().setClickable(true);
         if (getArguments() != null) {
-            gif = (Gif) getArguments().getSerializable("gif");
+            gif = (Gif) getArguments().getSerializable(KEY_GIF);
+            if (gif != null)
+                ImageUtils.loadGifIntoView(gif.getFullImage().url, binding.gifImage);
+            binding.gifUrl.setOnClickListener(v -> {
+                ClipboardManager clipboard = (ClipboardManager) requireActivity().getSystemService(v.getContext().CLIPBOARD_SERVICE);
+                if (clipboard != null) {
+                    ClipData clip = ClipData.newPlainText("Gif url", gif.url);
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(v.getContext(), "Url copied!", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
+
         return binding.getRoot();
     }
 
