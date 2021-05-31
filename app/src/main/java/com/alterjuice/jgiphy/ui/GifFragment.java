4bb1cc0e3e9a15c499cc5f23fd80128ed92b0557
AlterJuice <alterjuice1758@gmail.com>
2021-05-31 17:42:22 +0300

    Adding ViewModels to Fragments and linking the observers to ui. Now fragmentList manage the dependings of search query and if it is not empty - make search else showing gifs in trend. Rotation changings work without problems and saves the scrollPosition

--- app/src/main/java/com/alterjuice/jgiphy/ui/GifFragment.java
+++ app/src/main/java/com/alterjuice/jgiphy/ui/GifFragment.java
@@ -17,13 +17,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.databinding.BindingMethod;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import com.alterjuice.jgiphy.R;
 import com.alterjuice.jgiphy.databinding.GifFragmentBinding;
 import com.alterjuice.jgiphy.model.giphy.Gif;
+import com.alterjuice.jgiphy.viewmodel.GifViewModel;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.load.DataSource;
@@ -36,8 +37,10 @@ public class GifFragment extends Fragment {
     private static final String KEY_GIF = "arg:gif";
 
     private GifFragmentBinding binding;
-    private Gif gif;
+
     String TAG = "GifFragment";
+    private RequestManager grm;
+    private GifViewModel model;
 
 
     public static GifFragment newInstance(Gif gif) {
@@ -57,55 +60,77 @@ public class GifFragment extends Fragment {
             Log.d(TAG, "FragmentArguments null");
             return binding.getRoot();
         }
-        gif = (Gif) getArguments().getSerializable(KEY_GIF);
+
+        Gif gif = (Gif) getArguments().getSerializable(KEY_GIF);
+
         if (gif == null) {
             Log.d(TAG, "GifArgument is null");
             return binding.getRoot();
         }
-        binding.gifPerson.setVisibility(gif.hasUser() ? View.VISIBLE : View.GONE);
-        ConstraintSet set = new ConstraintSet();
-        set.clone(binding.gifLayoutImage.gifConstraint);
-        set.setDimensionRatio(binding.gifLayoutImage.gifImage.getId(), gif.getFullImage().getImageRatio());
-        set.applyTo(binding.gifLayoutImage.gifConstraint);
-        RequestManager grm = Glide.with(this);
-        grm.load(gif.getFullImage().url)
-                .thumbnail(grm.load(gif.images.originalStill.url))
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        Toast.makeText(getContext(), "Connection error", Toast.LENGTH_SHORT).show();
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        binding.gifProgress.setVisibility(View.GONE);
-                        return false;
-                    }
-                })
-                .fitCenter()
-                .skipMemoryCache(true)
-                .into(binding.gifLayoutImage.gifImage);
-
-
-        binding.gifUrl.setOnClickListener(v -> {
-            ClipboardManager clipboard = (ClipboardManager) requireActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-            if (clipboard != null) {
-                ClipData clip = ClipData.newPlainText("Gif " + gif.title + ": ", gif.url);
-                clipboard.setPrimaryClip(clip);
-                Toast.makeText(v.getContext(), "Url copied!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        binding.gifPerson.setOnClickListener(v -> {
-            if (gif.hasUser()) startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.user.urlProfile)));
-        });
-        binding.gifWeb.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.url))));
-        binding.gifBack.setOnClickListener(v -> requireActivity().onBackPressed());
-        binding.gifDownload.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.images.original.url))));
-
+        model = new GifViewModel(gif);
+        grm = Glide.with(this);
         return binding.getRoot();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        model.getGif().observe(getViewLifecycleOwner(), gifObserver);
+    }
+
+    private final Observer<Gif> gifObserver = new Observer<Gif>() {
+        @Override
+        public void onChanged(Gif gif) {
+            binding.gifPerson.setVisibility(gif.hasUser() ? View.VISIBLE : View.GONE);
+            ConstraintSet set = new ConstraintSet();
+            set.clone(binding.gifLayoutImage.gifConstraint);
+            set.setDimensionRatio(binding.gifLayoutImage.gifImage.getId(), gif.getFullImage().getImageRatio());
+            set.applyTo(binding.gifLayoutImage.gifConstraint);
+
+            grm.load(gif.getFullImage().url)
+                    .thumbnail(grm.load(gif.images.originalStill.url))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            Toast.makeText(getContext(), "Connection error", Toast.LENGTH_SHORT).show();
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            binding.gifProgress.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .fitCenter()
+                    .skipMemoryCache(true)
+                    .into(binding.gifLayoutImage.gifImage);
+
+
+            binding.gifUrl.setOnClickListener(v -> {
+                ClipboardManager clipboard = (ClipboardManager) requireActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                if (clipboard != null) {
+                    ClipData clip = ClipData.newPlainText("Gif " + gif.title + ": ", gif.url);
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(v.getContext(), "Url copied!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            binding.gifPerson.setOnClickListener(v -> {
+                if (gif.hasUser()) startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.user.urlProfile)));
+            });
+            binding.gifWeb.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.url))));
+            binding.gifBack.setOnClickListener(v -> requireActivity().onBackPressed());
+            binding.gifDownload.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(gif.images.original.url))));
+        }
+    };
 
     public void onBackPressed(View view) {
         if (view.getId() == R.id.gifBack || view.getId() == R.id.gifFrame) {
@@ -123,7 +148,6 @@ public class GifFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
-        binding.gifLayoutImage.gifImage.setImageDrawable(null);
         binding.unbind();
         binding = null;
         super.onDestroyView();
--- app/src/main/java/com/alterjuice/jgiphy/ui/GifListFragment.java
+++ app/src/main/java/com/alterjuice/jgiphy/ui/GifListFragment.java
@@ -2,6 +2,7 @@ package com.alterjuice.jgiphy.ui;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,11 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.SearchView;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.alterjuice.jgiphy.MainActivity;
 import com.alterjuice.jgiphy.R;
@@ -20,91 +22,41 @@ import com.alterjuice.jgiphy.adapters.GifAdapter;
 import com.alterjuice.jgiphy.adapters.OnBottomReachedListener;
 import com.alterjuice.jgiphy.adapters.OnGifClickedListener;
 import com.alterjuice.jgiphy.databinding.GifListFragmentBinding;
-import com.alterjuice.jgiphy.model.APIService;
-import com.alterjuice.jgiphy.model.TestConfig;
-import com.alterjuice.jgiphy.model.giphy.Gif;
-import com.alterjuice.jgiphy.model.giphy.response.SearchResponse;
-import com.alterjuice.jgiphy.model.giphy.response.TrendingResponse;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+import com.alterjuice.jgiphy.viewmodel.GifListViewModel;
 
 
 public class GifListFragment extends Fragment {
     public static final String TAG = "GifListFragment";
     GifListFragmentBinding binding;
     GifAdapter gifAdapter;
+    GifListViewModel model;
 
-    private final OnGifClickedListener onGifClickedListener = (gif, position) -> {
-        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)){
-            ((MainActivity) requireActivity()).showGif(gif);
-        }
-    };
-
-    private final OnBottomReachedListener onBottomReachedListener = () -> {
-
-    };
-    public static TrendingResponse trending;
-    APIService apiService;
-    public static final String BASE_URL = "http://api.giphy.com/";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.gif_list_fragment, container, false);
-        gifAdapter = prepareAdapter(getResources().getConfiguration().orientation);
+        boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        gifAdapter = new GifAdapter(isPortrait, onBottomReachedListener, onGifClickedListener);
         binding.recyclerGifs.setAdapter(gifAdapter);
-        test();
+        binding.appBar.appBarSearch.setOnQueryTextListener(searchQueryListener);
         return binding.getRoot();
-    }
-    public void test(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        apiService = retrofit.create(APIService .class);
-        apiService.getTrends(TestConfig.apiKey).enqueue(new Callback<TrendingResponse>() {
-            @Override
-            public void onResponse(Call<TrendingResponse> call, Response<TrendingResponse> response) {
-                if (response.body() == null) {
-                    Log.d(TAG, "Response Trending is null");
-                    return;
-                }
-                trending = response.body();
-                gifAdapter.update(trending.data);
-            }
-
-            @Override
-            public void onFailure(Call<TrendingResponse> call, Throwable t) { }
-        });
-        // apiService.searchGif(TestConfig.apiKey, "good morning").enqueue(new Callback<SearchResponse>() {
-        //     @Override
-        //     public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-        //         search = response.body();
-        //     }
-        //     @Override
-        //     public void onFailure(Call<SearchResponse> call, Throwable t) { }
-        // });
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
+        model = new ViewModelProvider(this).get(GifListViewModel.class);
+        model.getGifSearchQuery().observe(getViewLifecycleOwner(), s -> {
+            gifAdapter.clearItems();
+            Log.d(TAG, "GifAdapter Cleared. Setting Offset to 0");
+            model.loadGifs();
+        });
+        model.getGifs().observe(getViewLifecycleOwner(), gifs -> {
+            gifAdapter.update(gifs);
+            binding.executePendingBindings();
+        });
     }
-
-    public GifAdapter prepareAdapter(int configOrientation){
-        boolean orientationIsPortrait = configOrientation == Configuration.ORIENTATION_PORTRAIT;
-        return new GifAdapter(orientationIsPortrait, onBottomReachedListener, onGifClickedListener);
-    }
-
 
     @Override
     public void onDestroyView() {
@@ -113,4 +65,32 @@ public class GifListFragment extends Fragment {
         gifAdapter = null;
         super.onDestroyView();
     }
+
+    private final OnGifClickedListener onGifClickedListener = (gif, position) -> {
+        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)){
+            ((MainActivity) requireActivity()).showGif(gif);
+        }
+    };
+
+    private final OnBottomReachedListener onBottomReachedListener = () -> {
+        model.loadMoreGifs();
+    };
+
+    private final SearchView.OnQueryTextListener searchQueryListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            if (model.getGifSearchQuery().getValue() != query)
+                model.getGifSearchQuery().postValue(query);
+            binding.appBar.appBarSearch.clearFocus();
+            return false;
+        }
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            // Putting Trend Gifs to adapter if searchQuery is empty
+            if (TextUtils.isEmpty(newText))
+                if (!TextUtils.isEmpty(model.getGifSearchQuery().getValue()))
+                    model.getGifSearchQuery().postValue("");
+            return false;
+        }
+    };
 }
